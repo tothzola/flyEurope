b710942e99de75ca4903250a2cbbf3ebb3f466a7
Toth Zoltan <toth.gyula.zoltan@gmail.com>
2020-11-18 10:48:19 +0200

    project imlplementation
    project start,
    updated gitignore

---
+++ src/main/java/ro/zoltan/toth/fly_europe/FlyEuropeApplication.java
@@ -0,0 +1,13 @@
+package ro.zoltan.toth.fly_europe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyEuropeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyEuropeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/zoltan/toth/fly_europe/FlyEuropeApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.zoltan.toth.fly_europe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyEuropeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
