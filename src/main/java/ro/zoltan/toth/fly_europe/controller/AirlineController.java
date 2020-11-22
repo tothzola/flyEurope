c57044b62097820c1475a88988a18bf8d0b56af5
Toth Zoltan <toth.gyula.zoltan@gmail.com>
2020-11-22 20:16:19 +0200

    implement Flight;

--- src/main/java/ro/zoltan/toth/fly_europe/controller/AirlineController.java
+++ src/main/java/ro/zoltan/toth/fly_europe/controller/AirlineController.java
@@ -1,4 +1,11 @@
 package ro.zoltan.toth.fly_europe.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class AirlineController {
+
+    @RequestMapping({"/airlines"})
+    public String airlinesPage() {
+        return "airlines";
+    }
 }
--- src/main/java/ro/zoltan/toth/fly_europe/service/impl/AirlineServiceImpl.java
+++ src/main/java/ro/zoltan/toth/fly_europe/service/impl/AirlineServiceImpl.java
@@ -1,4 +1,7 @@
 package ro.zoltan.toth.fly_europe.service.impl;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AirlineServiceImpl {
 }
