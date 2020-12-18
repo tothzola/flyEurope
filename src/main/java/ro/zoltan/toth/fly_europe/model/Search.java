282dcbe3b9f76d6a92932152f67575a4c52bbc84
Toth Zoltan <toth.gyula.zoltan@gmail.com>
2020-12-18 18:11:58 +0200

    implementing Lombok to Search

--- src/main/java/ro/zoltan/toth/fly_europe/model/Search.java
+++ src/main/java/ro/zoltan/toth/fly_europe/model/Search.java
@@ -1,102 +1,25 @@
 package ro.zoltan.toth.fly_europe.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 public class Search {
 
-    private LocalDateTime departureTime;
-    private LocalDateTime arrivalTime;
-    private String callSign;
-    private String city;
-    private String code;
-    private String flightNo;
-    private String iata;
-    private String icao;
-    private String mapUrl;
-    private String name;
-
-    public LocalDateTime getDepartureTime() {
-        return departureTime;
-    }
-
-    public void setDepartureTime(LocalDateTime departureTime) {
-        this.departureTime = departureTime;
-    }
-
-    public LocalDateTime getArrivalTime() {
-        return arrivalTime;
-    }
-
-    public void setArrivalTime(LocalDateTime arrivalTime) {
-        this.arrivalTime = arrivalTime;
-    }
-
-    public String getCallSign() {
-        return callSign;
-    }
-
-    public void setCallSign(String callSign) {
-        this.callSign = callSign;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
+    @Getter @Setter private LocalDateTime departureTime;
+    @Getter @Setter private LocalDateTime arrivalTime;
+    @Getter @Setter private String callSign;
+    @Getter @Setter private String city;
+    @Getter @Setter private String code;
+    @Getter @Setter private String flightNo;
+    @Getter @Setter private String iata;
+    @Getter @Setter private String icao;
+    @Getter @Setter private String mapUrl;
+    @Getter @Setter private String name;
 
     public boolean hasCity() {
         return city != null && !city.isEmpty();
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getFlightNo() {
-        return flightNo;
-    }
-
-    public void setFlightNo(String flightNo) {
-        this.flightNo = flightNo;
-    }
-
-    public String getIata() {
-        return iata;
-    }
-
-    public void setIata(String iata) {
-        this.iata = iata;
-    }
-
-    public String getIcao() {
-        return icao;
-    }
-
-    public void setIcao(String icao) {
-        this.icao = icao;
-    }
-
-    public String getMapUrl() {
-        return mapUrl;
-    }
-
-    public void setMapUrl(String mapUrl) {
-        this.mapUrl = mapUrl;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public boolean hasName() {
